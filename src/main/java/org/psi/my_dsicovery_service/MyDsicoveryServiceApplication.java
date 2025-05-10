b13e34716c367350ad9f33c39c6adead145f6b2d
PSIR <79282746+psir466@users.noreply.github.com>
2025-05-10 20:55:58 +0200

    1s commit

---
+++ src/main/java/org/psi/my_dsicovery_service/MyDsicoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package org.psi.my_dsicovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MyDsicoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDsicoveryServiceApplication.class, args);
+	}
+
+}
